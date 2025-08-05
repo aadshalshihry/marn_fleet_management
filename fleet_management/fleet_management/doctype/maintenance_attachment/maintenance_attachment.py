66eb2bcaa73544cb68ebc0e8b463b4d5e39c126a
Abdulrahman Alshehri <79810965+aalshehri11@users.noreply.github.com>
2025-08-05 23:28:59 +0300

    Change the relationship with employee

---
+++ fleet_management/fleet_management/doctype/maintenance_attachment/maintenance_attachment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aalshehri and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MaintenanceAttachment(Document):
+	pass
