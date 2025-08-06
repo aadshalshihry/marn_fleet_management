7d71c430c07161cabc0697e4de952dd6ff05f4a5
Abdulrahman Alshehri <79810965+aalshehri11@users.noreply.github.com>
2025-08-06 21:33:08 +0300

    Remove Driver

--- fleet_management/fleet_management/doctype/driver/driver.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Aalshehri and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Driver", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- fleet_management/fleet_management/doctype/driver/driver.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Aalshehri and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Driver(Document):
-	pass
